6ba93864fedca07bbdba39d2190ca4c4c9c7146a
freddysilber <freddy.silber@cloudcoach.com>
2020-05-10 18:46:28 -0600

    upgrades seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Delete all skaters in database
+Skater.delete_all
+# Create some skaters
 Skater.create(
 	[
 		{first_name: 'CJ', last_name: 'Stohr', stance: 'Regular'},
